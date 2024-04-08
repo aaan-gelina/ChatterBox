201f5a511871a561a587ec838ad2462fe881582d
Noah <113081068+yiuunamm@users.noreply.github.com>
2024-04-07 22:39:12 -0700

    Create User Class
    user class with attributes from User on Firebase

---
+++ src/main/java/com/chatterbox/User.java
@@ -0,0 +1,26 @@
+package com.chatterbox;
+
+    public class User {
+    private String bio;
+    private String email;
+    private String fName;
+    private String lName;
+    private String phoneNumber;
+    private String status;
+    private String username;
+    public User() {}
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getfName() { return fName; }
+    public void setfName(String fName) { this.fName = fName; }
+    public String getlName() { return lName; }
+    public void setlName(String lName) { this.lName = lName; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+}
